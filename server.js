ee886f4e91499a724db40559b9ebb50397b6dd68
Clay <claystonebarongo@gmail.com>
2026-06-22 23:22:46 +0300

    a

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// Routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/inspections', inspectionRoutes);
