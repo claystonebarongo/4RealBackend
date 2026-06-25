0eeefa296805237c29cd374c33ebbbc9ac3dba0e
Clay <claystonebarongo@gmail.com>
2026-06-25 22:44:50 +0300

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
+
+
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
