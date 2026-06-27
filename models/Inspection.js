07c22172b1f09d9c0b0f6c6b1152c8589dea57bb
Clay <claystonebarongo@gmail.com>
2026-06-27 22:30:51 +0300

    favicon update

--- models/Inspection.js
+++ models/Inspection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const InspectionSchema = new mongoose.Schema({
     userId: {
+        
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
