19cdf35c5de4340e0252ec4870424c5fe23e751d
Clay <claystonebarongo@gmail.com>
2026-06-28 22:29:17 +0300

    update

--- server.js
+++ server.js
@@ -6,22 +6,32 @@ const authRoutes = require('./routes/authRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const inspectionRoutes = require('./routes/inspectionRoutes');
 
-
 dotenv.config();
-
 
 connectDB();
 
 const app = express();
 
+// Updated CORS configuration
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:5173'
+];
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl)
+        // or if the origin is in our allowed list
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 
 app.use(express.json());
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/payments', paymentRoutes);
